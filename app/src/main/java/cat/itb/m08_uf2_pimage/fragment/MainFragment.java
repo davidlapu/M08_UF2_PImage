51a1f58fd31649aec9bcb289119e2af9278b1ce2
David Lama Pulido <david.lama.7e3@itb.cat>
2021-02-02 19:08:41 +0100

    Initial commit

---
+++ app/src/main/java/cat/itb/m08_uf2_pimage/fragment/MainFragment.java
@@ -0,0 +1,28 @@
+package cat.itb.m08_uf2_pimage.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cat.itb.m08_uf2_pimage.R;
+
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
